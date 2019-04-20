ce55873041e2a6c059179c2fc26318b0a13ff135
Zimmermann Gyula <graiondilach@hotmail.com>
2019-04-20 23:59:07 +0200

    Implement FallsToEarthAS.

---
+++ OpenRA.Mods.AS/Activities/FallToEarthAS.cs
@@ -0,0 +1,67 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2015- OpenRA.Mods.AS Developers (see AUTHORS)
+ * This file is a part of a third-party plugin for OpenRA, which is
+ * free software. It is made available to you under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation. For more information, see COPYING.
+ */
+#endregion
+
+using System.Linq;
+using OpenRA.Activities;
+using OpenRA.Mods.AS.Traits;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.AS.Activities
+{
+	public class FallToEarthAS : Activity
+	{
+		readonly Aircraft aircraft;
+		readonly FallsToEarthASInfo info;
+		int acceleration = 0;
+		int spin = 0;
+		WDist velocity;
+
+		public FallToEarthAS(Actor self, FallsToEarthASInfo info)
+		{
+			this.info = info;
+			aircraft = self.Trait<Aircraft>();
+			velocity = info.Velocity;
+			if (info.Spins)
+			{
+				spin = info.SpinInitial;
+				acceleration = info.SpinAcceleration;
+			}
+		}
+
+		public override Activity Tick(Actor self)
+		{
+			if (self.World.Map.DistanceAboveTerrain(self.CenterPosition).Length <= 0)
+			{
+				if (info.ExplosionWeapon != null)
+				{
+					// Use .FromPos since this actor is killed. Cannot use Target.FromActor
+					info.ExplosionWeapon.Impact(Target.FromPos(self.CenterPosition), self, Enumerable.Empty<int>());
+				}
+
+				self.Dispose();
+				return null;
+			}
+
+			if (info.Spins)
+			{
+				spin += acceleration;
+				aircraft.Facing = (aircraft.Facing + spin) % 256;
+			}
+
+			var move = info.Moves ? aircraft.FlyStep(aircraft.Facing) : WVec.Zero;
+			velocity += info.VelocityAcceleration;
+			move -= new WVec(WDist.Zero, WDist.Zero, velocity);
+			aircraft.SetPosition(self, aircraft.CenterPosition + move);
+
+			return this;
+		}
+	}
+}
---
+++ OpenRA.Mods.AS/Traits/Air/FallsToEarthAS.cs
@@ -0,0 +1,47 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2015- OpenRA.Mods.AS Developers (see AUTHORS)
+ * This file is a part of a third-party plugin for OpenRA, which is
+ * free software. It is made available to you under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation. For more information, see COPYING.
+ */
+#endregion
+
+using OpenRA.GameRules;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Mods.AS.Activities;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.AS.Traits
+{
+	[Desc("Causes aircraft husks that are spawned in the air to crash to the ground.")]
+	public class FallsToEarthASInfo : ITraitInfo, IRulesetLoaded, Requires<AircraftInfo>
+	{
+		[WeaponReference]
+		public readonly string Explosion = "UnitExplode";
+
+		public readonly bool Spins = true;
+		public readonly int SpinInitial = 10;
+		public readonly int SpinAcceleration = 0;
+		public readonly bool Moves = false;
+		public readonly WDist Velocity = new WDist(0);
+		public readonly WDist VelocityAcceleration = new WDist(22);
+
+		public WeaponInfo ExplosionWeapon { get; private set; }
+
+		public object Create(ActorInitializer init) { return new FallsToEarthAS(init.Self, this); }
+		public void RulesetLoaded(Ruleset rules, ActorInfo ai)
+		{
+			ExplosionWeapon = string.IsNullOrEmpty(Explosion) ? null : rules.Weapons[Explosion.ToLowerInvariant()];
+		}
+	}
+
+	public class FallsToEarthAS
+	{
+		public FallsToEarthAS(Actor self, FallsToEarthASInfo info)
+		{
+			self.QueueActivity(false, new FallToEarthAS(self, info));
+		}
+	}
+}
