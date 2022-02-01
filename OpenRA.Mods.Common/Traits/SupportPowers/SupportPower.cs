51ed583fc28926bb0ae20c1c3c140efdf13cc0b1
darkademic <41052878+darkademic@users.noreply.github.com>
2022-02-01 19:32:20 +0000

    Costed support powers.

--- OpenRA.Mods.Common/Traits/SupportPowers/SupportPower.cs
+++ OpenRA.Mods.Common/Traits/SupportPowers/SupportPower.cs
@@ -39,6 +39,9 @@ namespace OpenRA.Mods.Common.Traits
 		[Desc("Allow this to be used only once.")]
 		public readonly bool OneShot = false;
 
+		[Desc("Cost to deploy.")]
+		public readonly int Cost = 0;
+
 		[CursorReference]
 		[Desc("Cursor to display for using this support power.")]
 		public readonly string Cursor = "ability";
--- OpenRA.Mods.Common/Widgets/ObserverSupportPowerIconsWidget.cs
+++ OpenRA.Mods.Common/Widgets/ObserverSupportPowerIconsWidget.cs
@@ -186,7 +186,7 @@ namespace OpenRA.Mods.Common.Widgets
 				lastIconIdx = i;
 				TooltipIcon = supportPowerIconsIcons[i];
 				currentTooltipToken = tooltipContainer.Value.SetTooltip(TooltipTemplate,
-					new WidgetArgs() { { "world", worldRenderer.World }, { "player", GetPlayer() }, { "getTooltipIcon", GetTooltipIcon } });
+					new WidgetArgs() { { "world", worldRenderer.World }, { "player", GetPlayer() }, { "getTooltipIcon", GetTooltipIcon }, { "playerResources", GetPlayer().PlayerActor.Trait<PlayerResources>() } });
 				return;
 			}
 
