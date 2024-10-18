b6858c3a32ad9e4e71b8ce558cc6102269129cf8
Patrick Wieth <patrick.wieth@googlemail.com>
2024-10-18 23:30:08 +0200

    add recoil to voxel turrets

--- OpenRA.Mods.Common/Traits/Render/WithVoxelTurret.cs
+++ OpenRA.Mods.Common/Traits/Render/WithVoxelTurret.cs
@@ -18,7 +18,7 @@ using OpenRA.Traits;
 
 namespace OpenRA.Mods.Common.Traits.Render
 {
-	public class WithVoxelTurretInfo : ConditionalTraitInfo, IRenderActorPreviewVoxelsInfo, Requires<RenderVoxelsInfo>, Requires<TurretedInfo>
+	public class WithVoxelTurretInfo : ConditionalTraitInfo, IRenderActorPreviewVoxelsInfo, Requires<RenderVoxelsInfo>, Requires<TurretedInfo>, Requires<ArmamentInfo>
 	{
 		[Desc("Voxel sequence name to use")]
 		public readonly string Sequence = "turret";
@@ -26,8 +26,14 @@ namespace OpenRA.Mods.Common.Traits.Render
 		[Desc("Turreted 'Turret' key to display")]
 		public readonly string Turret = "primary";
 
+		[Desc("Visual offset")]
+		public readonly WVec LocalOffset = WVec.Zero;
+
 		[Desc("Defines if the Voxel should have a shadow.")]
 		public readonly bool ShowShadow = true;
+
+		[Desc("Render recoil, should be activated when no VoxelBarrel is present (as this will also recoil)")]
+		public readonly bool Recoils = false;
 
 		public override object Create(ActorInitializer init) { return new WithVoxelTurret(init.Self, this); }
 
@@ -50,17 +56,43 @@ namespace OpenRA.Mods.Common.Traits.Render
 	public class WithVoxelTurret : ConditionalTrait<WithVoxelTurretInfo>
 	{
 		readonly Turreted turreted;
+		readonly Armament[] arms;
+		readonly BodyOrientation body;
 
 		public WithVoxelTurret(Actor self, WithVoxelTurretInfo info)
 			: base(info)
 		{
+			body = self.Trait<BodyOrientation>();
 			turreted = self.TraitsImplementing<Turreted>()
 				.First(tt => tt.Name == Info.Turret);
+			arms = self.TraitsImplementing<Armament>()
+				.Where(w => w.Info.Turret == info.Turret).ToArray();
 
 			var rv = self.Trait<RenderVoxels>();
 			rv.Add(new ModelAnimation(self.World.ModelCache.GetModelSequence(rv.Image, Info.Sequence),
-				() => turreted.Position(self), () => turreted.WorldOrientation,
+				() => TurretOffset(self), () => turreted.WorldOrientation,
 				() => IsTraitDisabled, () => 0, info.ShowShadow));
+		}
+
+		protected virtual WVec TurretOffset(Actor self)
+		{
+			if (!Info.Recoils)
+				return turreted.Position(self);
+			else{
+				// offset in turret coordinates
+				var localOffset = Info.LocalOffset;
+
+				foreach (var arm in arms) {
+					localOffset += new WVec(-arm.Recoil, WDist.Zero, WDist.Zero);
+				}
+
+				// Turret coordinates to body coordinates
+				var bodyOrientation = body.QuantizeOrientation(self.Orientation);
+				localOffset = localOffset.Rotate(turreted.WorldOrientation) + turreted.Offset.Rotate(bodyOrientation);
+
+				// Body coordinates to world coordinates
+				return body.LocalToWorld(localOffset);
+			}
 		}
 	}
 }
